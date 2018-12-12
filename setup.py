bd40a63ed6debfc30d2d7228cb26f527eedae2a4
Mr. Nayls <nayls@bk.ru>
2018-12-13 02:26:45 +0300

    chore(ci): up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="github-wrapper",
-    version="0.0.2",
+    version="0.0.3",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
