26208db356955deb8ecfdfb89413afebeee45ee7
Mr. Nayls <nayls@bk.ru>
2018-12-20 11:01:56 +0300

    fix: edit param all func

--- github_wrapper/v3/issues/Issues.py
+++ github_wrapper/v3/issues/Issues.py
@@ -13,7 +13,7 @@ def list_issues(params=None, auth=None) -> requests.Response:
     """
     return requests.get(
         "https://api.github.com/issues",
-        params,
+        params=params,
         auth=auth
     )
 
@@ -29,7 +29,7 @@ def list_user_issues(params=None, auth=None) -> requests.Response:
     """
     return requests.get(
         "https://api.github.com/user/issues",
-        params,
+        params=params,
         auth=auth
     )
 
@@ -46,7 +46,7 @@ def list_organization_issues(org: str, params=None, auth=None) -> requests.Respo
     """
     return requests.get(
         "https://api.github.com/orgs/{org}/issues".format(org=org),
-        params,
+        params=params,
         auth=auth
     )
 
@@ -63,7 +63,7 @@ def list_issues_for_a_repository(owner: str, repo: str, params=None, auth=None) 
     """
     return requests.get(
         "https://api.github.com/repos/{owner}/{repo}/issues".format(owner=owner, repo=repo),
-        params,
+        params=params,
         auth=auth
     )
 
@@ -86,19 +86,18 @@ def get_a_single_issue(owner: str, repo: str, number: int, params=None, auth=Non
     """
     return requests.get(
         "https://api.github.com/repos/{owner}/{repo}/issues/{number}".format(owner=owner, repo=repo, number=number),
-        params,
+        params=params,
         auth=auth
     )
 
 
-def create_an_issue(owner: str, repo: str, json=None, params=None, auth=None) -> requests.Response:
+def create_an_issue(owner: str, repo: str, json=None, auth=None) -> requests.Response:
     """
     Any user with pull access to a repository can create an issue.
 
     :param owner:
     :param repo:
     :param json:
-    :param params:
     :param auth:
     :return: :class:`Response <Response>` object
     :rtype: requests.Response
@@ -106,12 +105,11 @@ def create_an_issue(owner: str, repo: str, json=None, params=None, auth=None) ->
     return requests.post(
         "https://api.github.com/repos/{owner}/{repo}/issues".format(owner=owner, repo=repo),
         json=json,
-        params=params,
         auth=auth
     )
 
 
-def edit_an_issue(owner: str, repo: str, number: int, json=None, params=None, auth=None) -> requests.Response:
+def edit_an_issue(owner: str, repo: str, number: int, json=None, auth=None) -> requests.Response:
     """
     Issue owners and users with push access can edit an issue.
 
@@ -119,7 +117,6 @@ def edit_an_issue(owner: str, repo: str, number: int, json=None, params=None, au
     :param repo:
     :param number:
     :param json:
-    :param params:
     :param auth:
     :return: :class:`Response <Response>` object
     :rtype: requests.Response
@@ -131,21 +128,21 @@ def edit_an_issue(owner: str, repo: str, number: int, json=None, params=None, au
     )
 
 
-def lock_an_issue(owner: str, repo: str, number: int, params=None, auth=None) -> "Status: 204 No Content":
+def lock_an_issue(owner: str, repo: str, number: int, json=None, auth=None) -> "Status: 204 No Content":
     """
     Users with push access can lock an issue or pull request's conversation.
 
     :param owner:
     :param repo:
     :param number:
-    :param params:
+    :param json:
     :param auth:
     :return: :class:`Response <Response>` object
     :rtype: requests.Response
     """
     return requests.put(
         "https://api.github.com/repos/{owner}/{repo}/issues/{number}/lock".format(owner=owner, repo=repo, number=number),
-        params,
+        json=json,
         auth=auth
     )
 
